137d9a75ed0bab2abebbf4b314d6804a5bcfac53
Steven Leibrock <steven.leibrock@gmail.com>
2014-03-24 13:35:15 -0400

    Updating unit test

--- fcl/RectData.py
+++ fcl/RectData.py
@@ -1,33 +1,33 @@
-
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
 __author__ = 'Steven'
 
 import unittest
 import random
+from itertools import combinations
+
 
 class Rect(object):
     """
     Class to store rectangle information
     Stores (x,y,w,h), the type of Rect, and the ID of the rect
     """
-    def __init__(self, x, y, w, h, idtag="", typerect="text"):
+    def __init__(self, x, y, w, h, idtag="", typerect="text", value=""):
         self.x, self.y, self.w, self.h = x, y, w, h
         self.data = (x, y, w, h)
         self.idtag = idtag  # name tag of the Rect
         self.typerect = typerect  # default
+        self.value = value  # used for check/radio boxes
 
     def __iter__(self):
         return self.data
 
     def collide(self, rect):
-        """
-
-        :rtype : int
-        """
+        """Collision code"""
         pass
 
-
-
-class test_rect(unittest.TestCase):
+class TestRect(unittest.TestCase):
+    """Unit case for Rect class"""
 
     def setUp(self):
         self.seq = range(1000)
@@ -39,11 +39,11 @@ class test_rect(unittest.TestCase):
         Creates a rectangle with random placement and size.
         :rtype : rect
         """
-        x=random.choice(self.seq)
-        y=random.choice(self.seq)
-        w=random.choice(self.seq)
-        h=random.choice(self.seq)
-        return Rect(x,y,w,h)
+        x = random.choice(self.seq)
+        y = random.choice(self.seq)
+        w = random.choice(self.seq)
+        h = random.choice(self.seq)
+        return Rect(x, y, w, h)
 
     def add_recs(self):
         """
@@ -53,16 +53,11 @@ class test_rect(unittest.TestCase):
         for i in range(0,100,1):
             self.recs.append(self.create_rect())
 
-    def
-
     def test_collision(self):
         """
         Checks the rectangles for collisions
-
         """
         self.add_recs()
-        for i in range(0,self.recs.__sizeof__(),1):
-            for j in range(0,self.recs.__sizeof__(),1):
-                self.check = self.recs[i].collide(self.recs[j])
-
-        self.assertEqual(self.check, 0)
+        combos = combinations(self.recs, 2)
+        for pair in combos:
+            self.assertTrue(pair[0].collide(pair[1]))
