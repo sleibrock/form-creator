a04e340fbef43c5df7b8d0bf50ee71db8da9e078
andrew-pollitt <andrew.d.pollitt@gmail.com>
2014-03-24 13:23:47 -0400

    add unittest

--- fcl/RectData.py
+++ fcl/RectData.py
@@ -1,5 +1,8 @@
+
 __author__ = 'Steven'
 
+import unittest
+import random
 
 class Rect(object):
     """
@@ -16,4 +19,50 @@ class Rect(object):
         return self.data
 
     def collide(self, rect):
+        """
+
+        :rtype : int
+        """
         pass
+
+
+
+class test_rect(unittest.TestCase):
+
+    def setUp(self):
+        self.seq = range(1000)
+        self.check = 0
+        self.recs = []
+
+    def create_rect(self):
+        """
+        Creates a rectangle with random placement and size.
+        :rtype : rect
+        """
+        x=random.choice(self.seq)
+        y=random.choice(self.seq)
+        w=random.choice(self.seq)
+        h=random.choice(self.seq)
+        return Rect(x,y,w,h)
+
+    def add_recs(self):
+        """
+        Adds a number of rectangles to 'recs' for testing.
+        :param rect:
+        """
+        for i in range(0,100,1):
+            self.recs.append(self.create_rect())
+
+    def
+
+    def test_collision(self):
+        """
+        Checks the rectangles for collisions
+
+        """
+        self.add_recs()
+        for i in range(0,self.recs.__sizeof__(),1):
+            for j in range(0,self.recs.__sizeof__(),1):
+                self.check = self.recs[i].collide(self.recs[j])
+
+        self.assertEqual(self.check, 0)
